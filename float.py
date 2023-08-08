c9256253a842711885ee7ff5fad17ce1f0456828
Eniola011 <yettyagbalu@gmail.com>
2023-08-07 17:43:02 -0700

    convert float

---
+++ float.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# convert float number into 2 decimals
+# ask the user for a float number
+f = float(input("Enter a float number: "))
+# convert input
+print("Round to 2 decimals: {:.2f}".format(f))

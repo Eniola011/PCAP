a315426c3f76796165a1c5ed1048408a12ae9f0e
Eniola011 <yettyagbalu@gmail.com>
2023-08-07 17:17:49 -0700

    update:grades

--- grades.py
+++ grades.py
@@ -8,7 +8,7 @@ if (age == 5):
 # is the user between age 6 to 17?
 elif (age > 5) and (age <= 17):
     grade = age - 5
-    print("Go to {} grade".format(grade))
+    print("Go to grade {}".format(grade))
 # is user age greater than 17?
 else:
     print("Go to College.")
